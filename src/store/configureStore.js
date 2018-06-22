bb2600104660bdb399b9f2346bf950ade38c7d5d
Hunkyo Jung <hunkyo.jung@gmail.com>
2018-06-23 01:38:13 +0900

    Add redux-devtools-extension

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,7 +1,14 @@
 import { applyMiddleware, createStore } from 'redux';
 import { createLogger } from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import reducers from '../reducers';
 
 const logger = createLogger();
-export default createStore(reducers, undefined, applyMiddleware(logger));
+export default createStore(
+  reducers,
+  composeWithDevTools(
+    applyMiddleware(logger)
+    // other store enhancers if any
+  )
+);
